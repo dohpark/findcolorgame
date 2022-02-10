68ad29147ec82e038ed47e20de8cb2c3f42da32b
dohpark <parkdohee19931129@gmail.com>
2022-02-10 22:26:33 +0900

    feat: reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 import Header from "./components/Header";
 import { randomAnswerGenerator, randomRGBGenerator } from "./utils";
@@ -69,72 +69,8 @@ const App: React.FC = () => {
     answerBackgroundColor,
   });
 
-  const chooseAnswer = {
-    rightAnswer: () => {
-      setStage(stage + 1);
-      setScore(score + timeLeft * stage);
-      setTimeLeft(TIME_LEFT_INIT);
-    },
-    wrongAnswer: () => {
-      setTimeLeft(timeLeft - 3);
-    },
-  };
-
-  // array
-  let arrayCandidate = Array(block.arrayLength).fill({
-    backgroundColor: block.backgroundColor,
-    width: block.width,
-    onClickHandler: () => {
-      chooseAnswer.wrongAnswer();
-    },
-  });
-
-  arrayCandidate[block.answer] = {
-    backgroundColor: block.answerBackgroundColor,
-    width: block.width,
-    onClickHandler: () => {
-      chooseAnswer.rightAnswer();
-    },
-  };
-
-  const [array, setArray] = useState(arrayCandidate);
-
-  // stage별 block
-  useEffect(() => {
-    const {
-      width,
-      arrayLength,
-      backgroundColor,
-      answer,
-      answerBackgroundColor,
-    } = getBlock(stage);
-
-    setBlock({
-      width,
-      arrayLength,
-      backgroundColor,
-      answer,
-      answerBackgroundColor,
-    });
-  }, [stage]);
-
-  // timeLeft
-  useEffect(() => {
-    const id = setInterval(() => {
-      setTimeLeft(timeLeft - 1);
-    }, 1000);
-    if (timeLeft < 0) {
-      window.alert(`GAME OVER!\n스테이지: ${stage}, 점수: ${score}`);
-      setStage(STAGE_INIT);
-      setTimeLeft(TIME_LEFT_INIT);
-      setScore(SCORE_INIT);
-    }
-    return () => clearInterval(id);
-  }, [timeLeft, score, stage]);
-
-  // array
-  useEffect(() => {
-    // chooseAnswer
+  const createArray = useCallback(() => {
+    // choose answer
     const chooseAnswer = {
       rightAnswer: () => {
         setStage(stage + 1);
@@ -162,35 +98,53 @@ const App: React.FC = () => {
         chooseAnswer.rightAnswer();
       },
     };
+    return arrayCandidate;
+  }, [block, score, stage, timeLeft]);
 
-    setArray(arrayCandidate);
-  }, [timeLeft, block, score, stage]);
+  const [array, setArray] = useState(createArray());
+  const [reset, setReset] = useState(false);
 
-  /**
-   *
-   * 문제점
-   * arraylength 변경할 때 이때 answer또한 고정되어 있어서
-   * 변경할 때 전 answer이 같이 반영된 후에 변경 후의 answer이 반영됨.
-   * 정답 클릭 => stage 변경 => arraylength 변경 => array 변경 (answer은 변경하지 않음) => answer변경
-   *
-   * 해결방법
-   * 1. 둘의 프로세스가 분리되어 있기 때문에 발생됨. 둘을 하나로 합치는 방법이 가장 최선
-   *
-   * 진짜 문제점
-   * arrayWidth와 width를 state로 관리하지 않아, 내 허락 없이 알아서 바뀌어서 생긴 문제임
-   *
-   * 두번째 문제점
-   * 첫번째 렌더링 때 검은색 화면이 살짝 보인 후 바뀜
-   *
-   * 원인
-   * 첫 렌더링 때 검정색으로 구성된 화면이 나오는데 이를 먼저 보여줘서 생기는 문제
-   *
-   * 해결방법 1
-   * stage >1 과 reset 활용하면 될듯
-   *
-   *
-   *
-   */
+  // stage별 block 세팅
+  useEffect(() => {
+    if (stage > 1 || reset) {
+      const {
+        width,
+        arrayLength,
+        backgroundColor,
+        answer,
+        answerBackgroundColor,
+      } = getBlock(stage);
+
+      setBlock({
+        width,
+        arrayLength,
+        backgroundColor,
+        answer,
+        answerBackgroundColor,
+      });
+      setReset(false);
+    }
+  }, [stage, reset]);
+
+  // timeLeft
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+    if (timeLeft < 0) {
+      window.alert(`GAME OVER!\n스테이지: ${stage}, 점수: ${score}`);
+      setStage(STAGE_INIT);
+      setTimeLeft(TIME_LEFT_INIT);
+      setScore(SCORE_INIT);
+      setReset(true);
+    }
+    return () => clearInterval(id);
+  }, [timeLeft, score, stage]);
+
+  // array
+  useEffect(() => {
+    setArray(createArray());
+  }, [createArray]);
 
   return (
     <>
